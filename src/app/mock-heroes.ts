1789d094dc7125691c8f8c8fc21c603b74776b91
Adrian Garcia <131566206+itsmecramming@users.noreply.github.com>
2023-12-19 13:22:39 +0800

    Add files via upload

---
+++ src/app/mock-heroes.ts
@@ -0,0 +1,13 @@
+import { Hero } from './hero';
+
+export const HEROES: Hero[] = [
+  { id: 12, name: 'Dr. Nice' },
+  { id: 13, name: 'Superman' },
+  { id: 14, name: 'Wonderwoman' },
+  { id: 15, name: 'Aquaman' },
+  { id: 16, name: 'Flash' },
+  { id: 17, name: 'Cyborg' },
+  { id: 18, name: 'Dr. IQ' },
+  { id: 19, name: 'Magma' },
+  { id: 20, name: 'Tornado' }
+];
